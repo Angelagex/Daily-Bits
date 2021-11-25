a46b4c860564173ba727f99dd3a740bfe09a67bc
Angelagex <84979429+Angelagex@users.noreply.github.com>
2021-11-25 17:41:09 -0500

    Update href

--- scripts/loading.js
+++ scripts/loading.js
@@ -3,6 +3,6 @@ function redireccionar(){
     localStorage.setItem("totalesIncorrectas", 0);
     localStorage.setItem("horas", 0);
     localStorage.setItem("tiempoTotal", 0);
-    location.href="http://127.0.0.1:5500/Daily%20Bits/login.html";
+    location.href="https://angelagex.github.io/Daily-Bits/login.html";
 }
 setTimeout ("redireccionar()", 5000);
